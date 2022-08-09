8e76f6717bfa746382e18671a3b21b72ea618ab3
Lalbahadur786 <guptalalbahadur@gmail.com>
2022-08-09 09:59:22 +0530

    Bubble sort complete

--- sorting algorithms.py
+++ sorting algorithms.py
@@ -1,14 +1,22 @@
 # Bubble sort
-
-def bubble_sort(num_arr):
-    for i in range(0, len(num_arr)+1):
-        for j in range(0, len(num_arr) ):
-            if num_arr[i] > num_arr[j]
-            arr_num[i], arr_num[j] = arr_num[j], arr_num[i]
+def bubble_sort(arr_num):
+    for i in range(0, len(arr_num)-1):
+        print(f"pass ->{i}")
+        is_swapped = False
+        for j in range(0, len(arr_num)-1):
+            if arr_num[j] > arr_num[j+1]:
+                arr_num[j+1], arr_num[j] = arr_num[j], arr_num[j+1]
+                is_swapped = True
+                print(f"arr_content: {arr_num}")
+        if not is_swapped:
+            print("breaking the main loop due to no more swaps")
+            break
     return arr_num
 
+# insertion sort
 
 
 # Driver code
-arr_num = [1,2,4,6,2,8,5,7]
-print(arr_num)
+arr_num = [9,8,7,6,5,4,3,2,1]
+arr_num1 = [9,8,7,6,1,2,3,4,5]
+#print("final Bubble sort:",bubble_sort(arr_num1))
