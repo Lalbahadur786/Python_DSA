bad15898335222419afab5a0610141eb89f341d5
Lalbahadur786 <guptalalbahadur@gmail.com>
2022-08-09 09:26:36 +0530

    first commit

---
+++ sorting algorithms.py
@@ -0,0 +1,14 @@
+# Bubble sort
+
+def bubble_sort(num_arr):
+    for i in range(0, len(num_arr)+1):
+        for j in range(0, len(num_arr) ):
+            if num_arr[i] > num_arr[j]
+            arr_num[i], arr_num[j] = arr_num[j], arr_num[i]
+    return arr_num
+
+
+
+# Driver code
+arr_num = [1,2,4,6,2,8,5,7]
+print(arr_num)
