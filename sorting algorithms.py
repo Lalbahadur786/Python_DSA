28ca6018cca68d285b35f7ce448ba39bee6add53
Lalbahadur786 <guptalalbahadur@gmail.com>
2022-08-12 20:54:13 +0530

    added insertion sort

--- sorting algorithms.py
+++ sorting algorithms.py
@@ -1,4 +1,4 @@
-# Bubble sort
+# Bubble sort  in first full pass you largsted number wouldbe at end
 def bubble_sort(arr_num):
     for i in range(0, len(arr_num)-1):
         print(f"pass ->{i}")
@@ -13,10 +13,23 @@ def bubble_sort(arr_num):
             break
     return arr_num
 
-# insertion sort
+
+# insertion sort    will created sorted-unsorted sub array
+def  insertion_sort(arr_num):
+    for i in range(1,len(arr_num)):
+        key = arr_num[i]
+        for j in range(i):
+            if arr_num[j] > key:
+                del arr_num[i]
+                arr_num[j], arr_num[j+1:]  = key, arr_num[j:]
+                break
+                 
+    return arr_num
+
 
 
 # Driver code
 arr_num = [9,8,7,6,5,4,3,2,1]
 arr_num1 = [9,8,7,6,1,2,3,4,5]
 #print("final Bubble sort:",bubble_sort(arr_num1))
+print("final insertion sort:",insertion_sort(arr_num1))
