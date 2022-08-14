2ca39da2963c1e6585cbb512730cb7a9cb84d8ef
Lalbahadur786 <guptalalbahadur@gmail.com>
2022-08-14 22:26:45 +0530

    added selection sort and qucksort in recursive

---
+++ recursive sorting algos.py
@@ -0,0 +1,33 @@
+
+def partition(arr_num, low, high):
+    i = low -1
+    pivot = arr_num[high]
+    for j in range(low, high):
+        if arr_num[j] <= pivot:
+            i = i + 1
+            (arr_num[i], arr_num[j]) = (arr_num[j], arr_num[i])
+    (arr_num[i+1], arr_num[high]) = (arr_num[high], arr_num[i+1])
+    
+    return i + 1
+
+
+
+def quick_sort(arr_num, low, high):
+    if low < high:
+        pi = partition(arr_num, low, high)
+        quick_sort(arr_num, low, pi-1)
+        quick_sort(arr_num, pi+1, high)
+
+
+
+
+
+
+
+
+
+# Driver code
+arr_num = [9,8,7,6,5,4,3,2,1]
+arr_num1 = [5,4,3,2,1]
+quick_sort(arr_num1,0,len(arr_num1)-1)
+print(arr_num1)
--- sorting algorithms.py
+++ sorting algorithms.py
@@ -1,4 +1,4 @@
-# Bubble sort  in first full pass you largsted number wouldbe at end
+# Bubble sort  in first full pass you largest number would be at end
 def bubble_sort(arr_num):
     for i in range(0, len(arr_num)-1):
         print(f"pass ->{i}")
@@ -22,14 +22,26 @@ def  insertion_sort(arr_num):
             if arr_num[j] > key:
                 del arr_num[i]
                 arr_num[j], arr_num[j+1:]  = key, arr_num[j:]
-                break
-                 
+                break         
     return arr_num
 
 
+# selection sort 
+def selection_sort(arr_num):
+    for i in range(len(arr_num)):
+        min_value_index_j = i
+        for j in range(i+1,len(arr_num)):
+            if arr_num[j] < arr_num[min_value_index_j]:
+                min_value_index_j = j
+        #print(i,arr_num ,arr_num[min_value_index_j])
+        arr_num[i], arr_num[min_value_index_j] = arr_num[min_value_index_j], arr_num[i]
+    return arr_num
+
+        
 
 # Driver code
 arr_num = [9,8,7,6,5,4,3,2,1]
 arr_num1 = [9,8,7,6,1,2,3,4,5]
 #print("final Bubble sort:",bubble_sort(arr_num1))
-print("final insertion sort:",insertion_sort(arr_num1))
+#print("final insertion sort:",insertion_sort(arr_num1))
+#print("final selection sort:",selection_sort(arr_num1))
